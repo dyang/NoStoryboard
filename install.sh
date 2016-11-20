f2617fa0245a656e3fdc03a04565bb59e1567474
Derek Yang <deya01@handelsbanken.se>
2016-11-20 17:05:26 +0100

    Fix the error that installation might fail if target dir doesn't exist.

--- install.sh
+++ install.sh
@@ -4,5 +4,7 @@ set -e
 templateName="NoStoryboard"
 templateDir="$HOME/Library/Developer/Xcode/Templates/Project Templates/Custom"
 
+mkdir -p "$templateDir"
+
 rsync -r "$templateName.xctemplate" "$templateDir"
 echo "Copied to $templateDir"
